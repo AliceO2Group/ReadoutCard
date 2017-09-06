532d1deee461c607c832808800e6d5d76be54e5d
Pascal Boeschoten <passiepascal@gmail.com>
2017-09-06 14:36:27 +0200

    Add documentation

--- src/Register.h
+++ src/Register.h
@@ -3,8 +3,8 @@
 ///
 /// \author Pascal Boeschoten (pascal.boeschoten@cern.ch)
 
-#ifndef SRC_REGISTER_H_
-#define SRC_REGISTER_H_
+#ifndef ALICEO2_SRC_REGISTER_H_
+#define ALICEO2_SRC_REGISTER_H_
 
 #include <cstdint>
 #include <cstddef>
@@ -12,34 +12,43 @@
 namespace AliceO2 {
 namespace roc {
 
-/// A simple struct that holds an address and index of a BAR register
+/// A simple struct that holds an address and 32-bit index of a BAR register
+/// This is convenient because:
+///  - We generally program with the index
+///  - We'd like to initialize it with the address (the CRU people think in addresses and it makes it easier to check)
+///  - We occasionally need the address
 struct Register
 {
+    /// \param address Address of the register
     constexpr Register(uintptr_t address) noexcept : address(address), index(address / 4)
     {
     }
 
-    uintptr_t address; ///< byte-based address
-    size_t index; ///< 32-bit based index
+    const uintptr_t address; ///< byte-based address
+    const size_t index; ///< 32-bit based index
 };
 
 /// A simple struct that creates Register objects for registers that occur in intervals
+/// For example, the CRU has repeating registers for the multiple links. This allows us to describe that conveniently.
 struct IntervalRegister
 {
+    /// \param base Base address of the interval register
+    /// \param interval Interval of the register
     constexpr IntervalRegister(uintptr_t base, uintptr_t interval) noexcept : base(base), interval(interval)
     {
     }
 
+    /// \param Index of the register
     Register get(int index) const
     {
         return Register(base + interval * index);
     }
 
-    uintptr_t base; ///< Base address of register
-    uintptr_t interval; ///< Interval of register
+    const uintptr_t base; ///< Base address of register
+    const uintptr_t interval; ///< Interval of register
 };
 
 } // namespace roc
 } // namespace AliceO2
 
-#endif // SRC_REGISTER_H_
+#endif // ALICEO2_SRC_REGISTER_H_
