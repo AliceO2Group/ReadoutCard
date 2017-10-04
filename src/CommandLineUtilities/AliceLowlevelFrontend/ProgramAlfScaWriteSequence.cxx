110b34d3474325d783fd37a16be6836a3d249ab8
Pascal Boeschoten <passiepascal@gmail.com>
2017-10-04 18:07:14 +0200

    ALF: Add SCA sequence write utility

---
+++ src/CommandLineUtilities/AliceLowlevelFrontend/ProgramAlfScaWriteSequence.cxx
@@ -0,0 +1,85 @@
+/// \file ProgramAliceLowlevelFrontendClient.cxx
+/// \brief Utility that starts an example ALICE Lowlevel Frontend (ALF) DIM client
+///
+/// \author Pascal Boeschoten (pascal.boeschoten@cern.ch)
+
+#include "CommandLineUtilities/Program.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
+#include <thread>
+#include <dim/dic.hxx>
+#include "AliceLowlevelFrontend.h"
+#include "AlfException.h"
+#include "ServiceNames.h"
+
+using std::cout;
+using std::endl;
+
+namespace {
+using namespace AliceO2::roc::CommandLineUtilities;
+
+class ProgramAlfScaWriteSequence: public Program
+{
+  public:
+
+    virtual Description getDescription() override
+    {
+      return {"ALF SCA write sequence utility", "Writes a sequence of SCA commands from a file, with an optional output"
+        "file for returned results",
+        "roc-alf-write-seq --serial=12345 --file=/tmp/tpc-config.txt --out=/tmp/result.txt"};
+    }
+
+    virtual void addOptions(boost::program_options::options_description& options) override
+    {
+      Options::addOptionSerialNumber(options);
+      options.add_options()("file", boost::program_options::value<std::string>(&mFilePath)->required(),
+        "Path to command sequence file");
+      options.add_options()("out", boost::program_options::value<std::string>(&mOutFilePath),
+        "Path to output file. If not specified, will output to stdout");
+    }
+
+    virtual void run(const boost::program_options::variables_map& map) override
+    {
+      // Get DIM DNS node from environment
+      if (getenv(std::string("DIM_DNS_NODE").c_str()) == nullptr) {
+        BOOST_THROW_EXCEPTION(
+          Alf::AlfException() << Alf::ErrorInfo::Message("Environment variable 'DIM_DNS_NODE' not set"));
+      }
+
+      // Get program options
+      int serialNumber = Options::getOptionSerialNumber(map);
+
+      // Initialize DIM objects
+      Alf::ServiceNames names(serialNumber);
+      Alf::ScaWriteSequence scaWriteSequence(names.scaWriteSequence());
+
+      // Read file
+      std::ifstream file(mFilePath);
+      std::string commandSequence((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+      // Send commands to ALF
+      cout << "Writing command sequence of size " << commandSequence.size() << "...\n";
+      std::string result = scaWriteSequence.write(commandSequence);
+      cout << "Done!" << endl;
+
+      if (mOutFilePath.empty()) {
+        cout << "Received result: \n";
+        cout << "" << result << '\n';
+      } else {
+        std::ofstream outFile(mOutFilePath);
+        outFile << result;
+      }
+    }
+
+  private:
+    std::string mFilePath;
+    std::string mOutFilePath;
+};
+} // Anonymous namespace
+
+int main(int argc, char** argv)
+{
+  return ProgramAlfScaWriteSequence().execute(argc, argv);
+}
