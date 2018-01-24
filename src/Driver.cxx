dd6cb2cd086c3ec8440533fb948689025edd13d6
Pascal Boeschoten <passiepascal@gmail.com>
2018-01-24 16:06:02 +0100

    Add command error checking in freeUnusedChannelBuffers

--- src/Driver.cxx
+++ src/Driver.cxx
@@ -24,8 +24,13 @@ void initialize()
 
 // PDA keeps a handle to buffers that are registered to it.
 // This exists as a file "/sys/bus/pci/drivers/uio_pci_dma/[PCI address]/dma/[some number]/map"
+// This can be problematic when a readout process crashes without giving the driver the chance to deregister DMA buffer,
+// because then even if the readout's handle to the buffer is manually deleted, PDA's handle stays.
+// And if there's not enough memory to create a new buffer, we are stuck.
+//
+// But there's a way out.
 // If you echo that [some number] into "/sys/bus/pci/drivers/uio_pci_dma/[PCI address]/dma/free" it will be freed.
-// Not sure what happens if you do this while it's in use by a readout process...
+// Not sure what happens if you do this while it's in use by a readout process... But we check if it's in use.
 void freeUnusedChannelBuffers()
 {
   namespace bfs = boost::filesystem;
@@ -48,11 +53,13 @@ void freeUnusedChannelBuffers()
               if (bfs::is_directory(entry)) {
                 std::string mapPath = dmaPath + "/" + bufferId + "/map";
                 std::string freePath = dmaPath + "/free";
-                auto fuserResult = AliceO2::Common::System::executeCommand("fuser " + mapPath);
+                auto fuserResult = AliceO2::Common::System::executeCommand("fuser " + mapPath + " 2>&1");
                 if (fuserResult.empty()) {
                   // No process is using it, we can free the buffer!
                   logger << "Freeing PDA buffer '" + mapPath + "'" << InfoLogger::InfoLogger::endm;
-                  auto fuserResult = AliceO2::Common::System::executeCommand("echo " + bufferId + " > " + freePath);
+                  AliceO2::Common::System::executeCommand("echo " + bufferId + " > " + freePath);
+                } else {
+                  logger << "Not freeing PDA buffer '" + mapPath + "', fuser: '" + fuserResult + "'" << InfoLogger::InfoLogger::endm;
                 }
               }
             }
