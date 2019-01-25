ec980615dec124df6b2b10b51267fd859d4126c5
Kostas Alexopoulos <kostas.alexopoulos@cern.ch>
2019-01-25 16:49:48 +0100

    Use hash function that is deterministic across runs

--- include/ReadoutCard/InterprocessLock.h
+++ include/ReadoutCard/InterprocessLock.h
@@ -11,7 +11,7 @@
 #include <chrono>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <iostream>
+#include <Utilities/Util.h>
 
 #define LOCK_TIMEOUT 5 //5 second timeout in case we wait for the lock (e.g PDA)
 #define UNIX_SOCK_NAME_LENGTH 104 //108 for most UNIXs, 104 for macOS
@@ -73,11 +73,8 @@ class Lock
       if (mSocketName.length() >= UNIX_SOCK_NAME_LENGTH) {
         std::string lockType = mSocketName.substr(0, 17); // isolate the class that created the lock
                                                           // Alice_O2_RoC_XYZ_*
-        std::cout << "mSocketName: " << mSocketName << std::endl;
-        std::cout << "cstr: " << mSocketName.c_str() << std::endl;
-        std::size_t lockNameHash = std::hash<std::string>{} (mSocketName.c_str()); // hash the mutable part
+        unsigned long lockNameHash = Utilities::hashDjb2(mSocketName.c_str(), mSocketName.size()); // hash the mutable part
         std::string safeLockName = lockType + std::to_string(lockNameHash) + "_lock"; // return conformant name
-        std::cout << "lockNameHash: " << lockNameHash << std::endl;
         return safeLockName;
       } else {
         return mSocketName;
