b3c5fa083357606f9b13af91b9007346e12aacf8
Kostas Alexopoulos <kostas.alexopoulos@cern.ch>
2019-02-06 14:40:17 +0100

    Separate bar & swt stress tools

--- src/CommandLineUtilities/ProgramBarStress.cxx
+++ src/CommandLineUtilities/ProgramBarStress.cxx
@@ -20,6 +20,8 @@ using namespace AliceO2::roc;
 using namespace AliceO2::InfoLogger;
 namespace po = boost::program_options;
 
+constexpr uint32_t SWT_WR_WORD_L = 0x0f00040 / 4;
+
 class ProgramBarStress: public Program
 {
   public:
@@ -27,72 +29,40 @@ class ProgramBarStress: public Program
   virtual Description getDescription()
   {
     return {"Bar Stress", "Stress the Bar Accessor", 
-      "roc-bar-stress --id 42:00.0 --gbt-link 0 --cycles 100000 --print-freq 10000 --errorcheck"};
+      "roc-bar-stress --id 42:00.0 --cycles 100000 --print-freq 10000"};
   }
 
   virtual void addOptions(boost::program_options::options_description& options)
   {
     options.add_options()
-      ("gbt-link",
-       po::value<uint32_t>(&mOptions.gbtLink)->default_value(0),
-       "GBT link over which the bar writes will be performed. CRU is 0-17")
       ("cycles",
        po::value<long long>(&mOptions.cycles)->default_value(100),
-       "Cycles of SWT writes(/reads) to perform")
+       "Total bar writes to perform")
       ("print-freq",
        po::value<long long>(&mOptions.printFrequency)->default_value(10),
-       "Print every #print-freq cycles")
-      ("errorcheck",
-       po::bool_switch(&mOptions.errorCheck),
-       "Perform data validation");
+       "Print every #print-freq cycles");
     Options::addOptionCardId(options);
   }
 
-  int stress(Swt *swt, long long cycles, long long printFrequency, bool errorCheck) 
+  int stress(BarInterface *bar, long long cycles, long long printFrequency) 
   {
-
-    SwtWord swtWordWr = SwtWord(0x0, 0x0, 0x0);
-    SwtWord swtWordRd = SwtWord(0x0, 0x0, 0x0);
-
-    /* swtWordCheck for testing */
-    // SwtWord swtWordCheck = SwtWord(0x42, 0x42, 0x42);
 
     std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
     std::chrono::high_resolution_clock::time_point finish = std::chrono::high_resolution_clock::now();
 
     for (long long i=0;; i++){
-      swtWordWr.setLow((0x1 + i)%0xffffffff);
-      swtWordWr.setMed((0x2 + i)%0xffffffff); 
-      swtWordWr.setHigh((0x3 + i)%0xffff);
+      bar->writeRegister(SWT_WR_WORD_L, 0x42);
       
-      uint32_t mon = swt->write(swtWordWr);
-      if (isVerbose())
-        getLogger() << "WR MON: 0x" << std::setfill('0') << std::hex << mon << InfoLogger::endm;
-     
-      if (errorCheck){
-        uint32_t mon = swt->read(swtWordRd);
-        if (swtWordRd != swtWordWr){
-          getLogger() << "SWT validation failed" << InfoLogger::endm;
-          getLogger() << "Read: " << swtWordRd << " | Expected: " << swtWordWr << InfoLogger::endm;
-          return -1;
-        }
-        
-        if (isVerbose()){
-          getLogger() << "RD MON: 0x" << std::setfill('0') << std::hex << mon << InfoLogger::endm;
-          getLogger() << "Read swtWord: " << swtWordRd << InfoLogger::endm;
-        }
-      }
-
-      if (i && (i%printFrequency == 0)){
+      if (i && ((i%printFrequency == 0) || (i == cycles))){
         finish = std::chrono::high_resolution_clock::now();
         getLogger() << "loops [" << i-printFrequency+1 << " - " << i  << "]: " <<
           std::chrono::duration_cast<std::chrono::nanoseconds> (finish - start).count() << "ns" << InfoLogger::endm;
 
         if (i == cycles || isSigInt()){ //sigInt only stops at cycles = printFrequency * X
-          double throughput = (barOps /(std::chrono::duration_cast<std::chrono::nanoseconds> (finish - start).count() * 1e-9)); //throughput (ops/time) [ops/sec]
+          double throughput = (printFrequency /(std::chrono::duration_cast<std::chrono::nanoseconds> (finish - start).count() * 1e-9)); //throughput (ops/time) [ops/sec]
           getLogger() << "Throughput :" << throughput << " ops/sec" << InfoLogger::endm;
 
-          double latency = ((std::chrono::duration_cast<std::chrono::nanoseconds> (finish - start).count() * 1e-9) / barOps); // latency (time/ops) [sec]
+          double latency = ((std::chrono::duration_cast<std::chrono::nanoseconds> (finish - start).count() * 1e-9) / printFrequency); // latency (time/ops) [sec]
           getLogger() << "Operation latency: " << latency << " sec " << InfoLogger::endm;
           return i;
         }
@@ -108,19 +78,9 @@ class ProgramBarStress: public Program
     auto cardId = Options::getOptionCardId(map);
 
     getLogger() << "Card ID: " << cardId << InfoLogger::endm;
-    getLogger() << "GBT Link: " << mOptions.gbtLink << InfoLogger::endm;
-    getLogger() << "Cycles of SWT write(/read) operations: " << mOptions.cycles << InfoLogger::endm;
+    getLogger() << "Total BAR write operations: " << mOptions.cycles << InfoLogger::endm;
     getLogger() << "Print frequency: " << mOptions.printFrequency << InfoLogger::endm;
-    getLogger() << "Error Check enabled: " << mOptions.errorCheck << InfoLogger::endm;
 
-    /* bar(Writes | Reads | Ops) every #printFrequency cycles */
-    barWrites = (SWT_WRITE_BAR_WRITES + SWT_READ_BAR_WRITES*mOptions.errorCheck)*mOptions.printFrequency;
-    barReads = (SWT_WRITE_BAR_READS + SWT_READ_BAR_READS*mOptions.errorCheck)*mOptions.printFrequency;
-    barOps = barWrites + barReads;
-
-    getLogger() << "Logging time every " << barOps << " bar operations, of which:" << InfoLogger::endm;
-    getLogger() << "barWrites: " << barWrites << " | barReads: " << barReads << InfoLogger::endm;
-      
     std::shared_ptr<BarInterface>
       bar0 = ChannelFactory().getBar(cardId, 0);
     std::shared_ptr<BarInterface>
@@ -131,36 +91,24 @@ class ProgramBarStress: public Program
     bar0->writeRegister(Cru::Registers::RESET_CONTROL.index, 0x1);
 
     if(isVerbose())
-      getLogger() << "Initializing SWT..." << InfoLogger::endm;
-    auto swt = Swt(*bar2, mOptions.gbtLink);
-
-    if(isVerbose())
       getLogger() << "Running operations..." << InfoLogger::endm;
   
     auto start = std::chrono::high_resolution_clock::now();
-    long long cycles_run = stress(&swt, mOptions.cycles, mOptions.printFrequency, mOptions.errorCheck);
+    long long cycles_run = stress(bar2.get(), mOptions.cycles, mOptions.printFrequency);
     auto finish = std::chrono::high_resolution_clock::now();
     
     if (!cycles_run)
       getLogger() << "Execution terminated because of error..." << InfoLogger::endm;
     
     getLogger() << "Total duration: " << std::chrono::duration_cast<std::chrono::seconds> (finish-start).count() << "s" << InfoLogger::endm;
-    getLogger() << "Total bar operations: " << barOps * (cycles_run/mOptions.printFrequency) << InfoLogger::endm;
-    getLogger() << "Total bar writes " << barWrites * (cycles_run/mOptions.printFrequency) << InfoLogger::endm;
-    getLogger() << "Total bar reads: " << barReads * (cycles_run/mOptions.printFrequency) << InfoLogger::endm;
+    getLogger() << "Total bar operations: " << cycles_run << InfoLogger::endm;
   }
 
   struct OptionsStruct 
   {
-    uint32_t gbtLink = 0;
     long long cycles = 100;
     long long printFrequency = 10;
-    bool errorCheck = true;
   }mOptions;
-
-  long long barOps = 0;
-  long long barWrites = 0;
-  long long barReads = 0;
 
   private:
 };
